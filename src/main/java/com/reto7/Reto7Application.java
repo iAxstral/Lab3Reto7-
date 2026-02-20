e25954377e685d7d5c05c1f56ee85688eab574be
Tomas <tomasdi.dev@gmail.com>
2026-02-19 21:17:12 -0500

    Spring Boot arreglado

---
+++ src/main/java/com/reto7/Reto7Application.java
@@ -0,0 +1,13 @@
+package com.reto7;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Reto7Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Reto7Application.class, args);
+	}
+
+}
